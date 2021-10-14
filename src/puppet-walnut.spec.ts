f1c45da343dd30a5cba6ebc39d27846b37829096
张瑞宁 <3134191406@qq.com>
2021-10-14 20:02:56 +0800

    modify puppet-walnut.spec.ts

--- src/puppet-walnut.spec.ts
+++ src/puppet-walnut.spec.ts
@@ -1,28 +1,26 @@
 #!/usr/bin/env ts-node
 
-import test  from 'blue-tape'
+import test  from 'tstest'
 
 import { PuppetWalnut } from './puppet-walnut'
 
-class PuppetWalnutTest extends PuppetWalnut {
-}
+/**
+ *   zrn-fight - https://github.com/zrn-fight
+ *
+ *   2021.10.14
+ *
+ *   This test can pass CI.
+ *   The job running on runner Hosted Agent will exceede the maximum execution time of 360 minutes.
+ *
+ *   issue:https://github.com/wechaty/wechaty-puppet-walnut/issues/9
+ *
+ */
 
-test.skip('Puppet5gmsg perfect restart testing', async (t) => {
-  const puppet = new PuppetWalnutTest({ sms: '12345' })
-  try {
-
-    for (let i = 0; i < 3; i++) {
-      await puppet.start()
-      t.true(puppet.state.on(), 'should be turned on after start()')
-
-      await puppet.stop()
-      t.true(puppet.state.off(), 'should be turned off after stop()')
-
-      t.pass('start/stop-ed at #' + i)
-    }
-
-    t.pass('PuppetWalnut() perfect restart pass.')
-  } catch (e) {
-    t.fail(e)
+test.skip('PuppetWalnut perfect restart testing', async (t) => {
+  const puppet = new PuppetWalnut({ sms: '12345' })
+  for (let n = 0; n < 3; n++) {
+    await puppet.start()
+    await puppet.stop()
+    t.pass('perfect restart succeed at #' + n)
   }
 })

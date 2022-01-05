b98bfc680f3cc937e3127dced306e00e0304e4a1
fabian4 <fabian4@163.com>
2022-01-05 21:46:12 +0800

    feat: implement contact function

--- examples/chatbot.ts
+++ examples/chatbot.ts
@@ -2,8 +2,14 @@ import { WechatyBuilder } from 'wechaty'
 import { log } from 'wechaty-puppet'
 import PuppetWalnut from '../src/puppet-walnut.js'
 
-WechatyBuilder.build({
+const bot = WechatyBuilder.build({
   puppet: new PuppetWalnut(),
 })  // get a Wechaty instance
   .on('login',            user => log.info(`User ${user} logged in`))
   .on('message',       message => log.info(`Message: ${message}`))
+
+await bot.start()
+
+const contact = await bot.Contact.find({ id: '15751763183' })
+contact.say('hello')
+console.log(contact)

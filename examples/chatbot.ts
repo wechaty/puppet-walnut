86209248a6c7a270fe887bf0df2f48db0be805f7
fabian4 <fabian4@163.com>
2022-01-20 20:27:08 +0800

    feat: implement message function

--- examples/chatbot.ts
+++ examples/chatbot.ts
@@ -1,8 +1,6 @@
 import { WechatyBuilder } from 'wechaty'
 import { log } from 'wechaty-puppet'
 import PuppetWalnut from '../src/puppet-walnut.js'
-import { FileBox } from 'file-box'
-
 const bot = WechatyBuilder.build({
   puppet: new PuppetWalnut(),
 })  // get a Wechaty instance
@@ -15,15 +13,15 @@ const bot = WechatyBuilder.build({
 await bot.start()
 
 const contact = await bot.Contact.find({ id: '15751763183' })
-// const location = {
-//   accuracy  : 11,
-//   address   : '北京市北京市海淀区45 Chengfu Rd',
-//   latitude  : 39.995120999999997,
-//   longitude : 116.334154,
-//   name      : '东升乡人民政府(海淀区成府路45号)',
-// }
-// await contact.say(location)
-contact.say(FileBox.fromUrl('https://fabian.oss-cn-hangzhou.aliyuncs.com/img/mmexport1630917534919.jpg'))
+const location = {
+  accuracy  : 11,
+  address   : '北京市北京市海淀区45 Chengfu Rd',
+  latitude  : 39.995120999999997,
+  longitude : 116.334154,
+  name      : '东升乡人民政府(海淀区成府路45号)',
+}
+await contact.say(location)
+// contact.say(FileBox.fromUrl('https://fabian.oss-cn-hangzhou.aliyuncs.com/img/mmexport1630917534919.jpg'))
 // console.log(msg)
 // contact.sync()
 // const message = await bot.Message.find({ id: '8dcf7a26-5760-103a-b02f-f7e6c49198e7' })

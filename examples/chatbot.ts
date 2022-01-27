97c43b7e2b482ab4cb84c5b06cc165f78e48c5e2
fabian4 <fabian4@163.com>
2022-01-27 12:01:12 +0800

    feat: support kinds of messages

--- examples/chatbot.ts
+++ examples/chatbot.ts
@@ -1,19 +1,19 @@
-import {WechatyBuilder} from 'wechaty'
-import { log } from 'wechaty-puppet'
-import PuppetWalnut from '../src/puppet-walnut.js'
+// import {WechatyBuilder} from 'wechaty'
+// import { log } from 'wechaty-puppet'
+// import PuppetWalnut from '../src/puppet-walnut.js'
 // import { FileBox } from 'file-box'
-const bot = WechatyBuilder.build({
-  puppet: new PuppetWalnut(),
-})  // get a Wechaty instance
-  .on('login',            user => log.info(`User ${user} logged in`))
-  .on('message',       async message => {
-    log.info(`Message: ${message}`)
-    console.log(message)
-    const file = await message.toFileBox()
-    console.log(file)
-  })
-
-await bot.start()
+// const bot = WechatyBuilder.build({
+//   puppet: new PuppetWalnut(),
+// })  // get a Wechaty instance
+//   .on('login',            user => log.info(`User ${user} logged in`))
+//   .on('message',       async message => {
+//     log.info(`Message: ${message}`)
+//     console.log(message)
+//     const file = await message.toFileBox()
+//     console.log(file)
+//   })
+//
+// await bot.start()
 
 // const contact = await bot.Contact.find({ id: '15751763183' })
 // contact.say(new LocationInterface('payload'))
@@ -31,3 +31,22 @@ await bot.start()
 // const message = await bot.Message.find({ id: '8dcf7a26-5760-103a-b02f-f7e6c49198e7' })
 // console.log(message.age())
 // console.log(message.date())
+
+import { parseVCards } from 'vcard4-ts'
+import { readFileSync } from 'fs'
+import * as util from "util";
+
+const vcf = readFileSync('C:\\Users\\fabian\\Desktop\\1.vcf').toString()
+
+const cards = parseVCards(vcf)
+if (cards.vCards) {
+  let card = cards.vCards[0]
+  // console.log(util.inspect(card))
+  // console.log(JSON.stringify(card))
+  console.log(card.TEL[0].value)
+  // for (const card of cards.vCards) {
+  //   console.log('Card found for ' + card.FN[0].value[0])
+  // }
+} else {
+  console.error('No valid vCards in file')
+}

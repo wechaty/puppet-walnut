b3f755ab059306887dc30b88e5cc6a3b333c1c9f
fabian4 <fabian4@163.com>
2022-02-08 12:03:27 +0800

    feat: add unit test

--- src/cache/cacheManager.spec.ts
+++ src/cache/cacheManager.spec.ts
@@ -1,17 +1,7 @@
 #!/usr/bin/env -S node --no-warnings --loader ts-node/esm
 
 import { test } from 'tstest'
-import PuppetWalnut from '../puppet-walnut.js'
 
 test('cacheManager test', async t => {
-  // await new PuppetWalnut({
-  //   appId: '28871d8c83954bc78424ffcbff80285c',
-  //   appKey: '3b9cc5506af2466aa82eee4c04f86471',
-  //   sipId: '20210401',
-  // }).start()
-  // const cacheManager = PuppetWalnut.getCacheManager()
   t.pass()
-  // const cache1 = CacheManager.Instance
-  // const cache2 = CacheManager.Instance
-  // t.equal(cache1, cache2)
 })

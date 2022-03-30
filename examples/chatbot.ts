8fdd0f2ef46a54fb1c7465c52cd72a4f5951650d
fabian4 <fabian4@163.com>
2022-03-30 18:42:40 +0800

    feat: add test

--- examples/chatbot.ts
+++ examples/chatbot.ts
@@ -1,22 +1,22 @@
-import {WechatyBuilder} from 'wechaty'
+import { WechatyBuilder } from "wechaty"
 import { log } from 'wechaty-puppet'
 import PuppetWalnut from '../src/puppet-walnut.js'
 // import { FileBox } from 'file-box'
 const bot = WechatyBuilder.build({
   puppet: new PuppetWalnut(),
 })  // get a Wechaty instance
-  .on('login',            user => log.info(`User ${user} logged in`))
-  .on('message',       async message => {
+  .on('login',            (user: any) => log.info(`User ${user} logged in`))
+  .on('message',       async (message: { toContact: () => void }) => {
     log.info(`Message: ${message}`)
     message.toContact()
     console.log(message)
-    const file = await message.toContact()
-    console.log(file)
+    // const file = await message.toContact()
+    // console.log(file)
   })
 
 await bot.start()
-//
-// const contact = await bot.Contact.find({ id: '15751763183' })
+
+const contact = await bot.Contact.find({ id: '15751763183' })
 // contact.say(new LocationInterface('payload'))
 // const location = {
 //   accuracy  : 11,
@@ -25,19 +25,10 @@ await bot.start()
 //   longitude : 116.334154,
 //   name      : '东升乡人民政府(海淀区成府路45号)',
 // }
-// await contact.say('aaa')
+await contact.say('aaa')
 // contact.say(FileBox.fromUrl('https://fabian.oss-cn-hangzhou.aliyuncs.com/img/mmexport1630917534919.jpg'))
 // console.log(msg)
 // contact.sync()
 // const message = await bot.Message.find({ id: '8dcf7a26-5760-103a-b02f-f7e6c49198e7' })
 // console.log(message.age())
 // console.log(message.date())
-
-import PuppetWalnut from '../src/puppet-walnut.js'
-
-await new PuppetWalnut({
-  appId: '28871d8c83954bc78424ffcbff80285c',
-  appKey: '3b9cc5506af2466aa82eee4c04f86471',
-  sipId: '20210401',
-}).start()
-const cacheManager = PuppetWalnut.getCacheManager()

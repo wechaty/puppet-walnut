4435f12bf60bbc30a44d61658c045cf638522ebc
fabian4 <fabian4@163.com>
2022-04-15 17:34:28 +0800

    feat: add examples

--- examples/chatbot.ts
+++ examples/chatbot.ts
@@ -17,16 +17,16 @@ await bot.start()
 
 const contact = await bot.Contact.find({ id: '15751763183' })
 
-// const post = await bot.Post.builder()
-//   .add('This is a single rich card.')
-//   .add('This is the description of the rich card. It\'s the first field that will be truncated if it exceeds the maximum width or height of a card.')
-//   .add(FileBox.fromFile('C:\\Users\\fabian\\Desktop\\1.png'))
-//   .type(PUPPET.types.Post.Unspecified)
-//   .build()
-//
-// await contact.say(post)
+const post = await bot.Post.builder()
+  .add('This is a single rich card.')
+  .add('This is the description of the rich card. It\'s the first field that will be truncated if it exceeds the maximum width or height of a card.')
+  // .add(FileBox.fromUrl('https://fabian.oss-cn-hangzhou.aliyuncs.com/img/mmexport1630917534919.jpg'))
+  .add(FileBox.fromFile('C:\\Users\\fabian\\Desktop\\1.png'))
+  .type(PUPPET.types.Post.Unspecified)
+  .build()
 
-// contact.say(new LocationInterface('payload'))
+await contact.say(post)
+
 // const location = {
 //   accuracy  : 11,
 //   address   : '北京市北京市海淀区45 Chengfu Rd',
@@ -34,7 +34,9 @@ const contact = await bot.Contact.find({ id: '15751763183' })
 //   longitude : 116.334154,
 //   name      : '东升乡人民政府(海淀区成府路45号)',
 // }
-await contact.say('This is a simple text message.')
+// await contact.say(location)
+
+// await contact.say('This is a simple text message.')
 // contact.say(FileBox.fromUrl('https://fabian.oss-cn-hangzhou.aliyuncs.com/img/mmexport1630917534919.jpg'))
 // console.log(msg)
 // contact.sync()
